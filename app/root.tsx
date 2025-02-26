e462c452bc108b17f8bf7116ca3d76ad5ed5e289
Tomasz Frankowski <tomasz@placeholderstud.io>
2025-02-26 21:28:00 +0100

    Add favicon and web app manifest

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,12 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+  { rel: "shortcut icon", href: "/favicon/favicon.ico" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+  { rel: "manifest", href: "/favicon/site.webmanifest" },
+
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
